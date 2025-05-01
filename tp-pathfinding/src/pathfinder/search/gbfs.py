57dcd2339dcf0cca6bad539c97755cce219731dc
Maximiliano Urtubey <urtubey@gmail.com>
2025-05-01 18:38:33 -0300

    Completo el algoritmo GBFS de grafo

--- tp-pathfinding/src/pathfinder/search/gbfs.py
+++ tp-pathfinding/src/pathfinder/search/gbfs.py
@@ -3,7 +3,6 @@ from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
-
 class GreedyBestFirstSearch:
     @staticmethod
     def search(grid: Grid) -> Solution:
@@ -15,13 +14,52 @@ class GreedyBestFirstSearch:
         Returns:
             Solution: Solution found
         """
+        # Función Heurística que devuelve la distancia de
+        # Manhattan desde el nodo actual al 'grid.end'
+        def heuristica(nodo_act: Node) -> int:
+            return abs(nodo_act.state[0] - grid.end[0]) + abs(nodo_act.state[1] - grid.end[1])
+        
         # Initialize a node with the initial position
         node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
+        # Inicializa frontera como una cola de prioridad
+        frontera = PriorityQueueFrontier()
+
+        # Agrega el nodo y su heurística a la frontera
+        frontera.add(node, heuristica(node))
         
-        # Add the node to the explored dictionary
-        explored[node.state] = True
+        # Inicializa alcanzados como diccionario vacío
+        alcanzados = {} 
         
-        return NoSolution(explored)
+        # Agrega el estado y el costo a alcanzados
+        alcanzados[node.state] = node.cost
+
+        while True:
+
+            # Falla si la frontera está vacía
+            if frontera.is_empty():
+                return NoSolution(alcanzados)
+            
+            # Quita al nodo de menor costo de la frontera
+            nodo = frontera.pop()
+            
+            # Verifica si el estado del nodo es el 
+            # estado-objetivo
+            if nodo.state == grid.end:
+                return Solution(nodo, alcanzados)
+            
+            # Alamacena las posibles acciones en el
+            # diccionario vecinos
+            vecinos = grid.get_neighbours(nodo.state)
+
+            # Evalúa las acciones guardadas en vecinos
+            for accion in vecinos:
+                nuevo_estado = vecinos[accion]
+                nuevo_costo = nodo.cost + grid.get_cost(nuevo_estado)
+                # Descarta únicamente los nodos que fueron alcanzados con
+                # un consto de camino menor
+                if nuevo_estado not in alcanzados or nuevo_costo < alcanzados[nodo.state]:
+                    nuevo_nodo = Node('', nuevo_estado, nuevo_costo, nodo, accion)
+                    alcanzados[nuevo_estado] = nuevo_costo
+                    frontera.add(nuevo_nodo, heuristica(nuevo_nodo))
+        # return NoSolution(explored)
