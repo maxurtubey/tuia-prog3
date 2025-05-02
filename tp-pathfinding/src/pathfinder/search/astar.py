7c8c97c4d9d53790cede95f6ba77065c14d44606
Maximiliano Urtubey <urtubey@gmail.com>
2025-05-02 17:10:49 -0300

    Se completa el algoritmo A* de grafo

--- tp-pathfinding/src/pathfinder/search/astar.py
+++ tp-pathfinding/src/pathfinder/search/astar.py
@@ -15,13 +15,51 @@ class AStarSearch:
         Returns:
             Solution: Solution found
         """
+        # Función Heurística que devuelve la distancia de
+        # Manhattan desde el nodo actual al 'grid.end'
+        def heuristica(nodo_act: Node) -> int:
+            return abs(nodo_act.state[0] - grid.end[0]) + abs(nodo_act.state[1] - grid.end[1])
+        
         # Initialize a node with the initial position
         node = Node("", grid.start, 0)
 
-        # Initialize the explored dictionary to be empty
-        explored = {} 
+        # Inicializa la frontera como una cola de prioridad
+        frontera = PriorityQueueFrontier()
+
+        # Agrega el nodo y la suma de su costo más 
+        # su heurística a la frontera
+        frontera.add(node, node.cost + heuristica(node))
         
-        # Add the node to the explored dictionary
-        explored[node.state] = True
+        # Inicializa alcanzados como diccionario vacío
+        alcanzados = {} 
+
+        # Agrega el estado y el costo a alcanzados
+        alcanzados[node.state] = node.cost
+
+        while frontera:
+
+            # Quita al nodo de menor costo de la frontera
+            nodo = frontera.pop()
+            
+            # Verifica si el estado del nodo es el 
+            # estado-objetivo
+            if nodo.state == grid.end:
+                return Solution(nodo, alcanzados)
+            
+            # Alamacena las posibles acciones en el
+            # diccionario vecinos
+            vecinos = grid.get_neighbours(nodo.state)
+
+            # Evalúa las acciones guardadas en vecinos
+            for accion in vecinos:
+                nuevo_estado = vecinos[accion]
+                nuevo_costo = nodo.cost + grid.get_cost(nuevo_estado)
+                # Descarta únicamente los nodos que fueron alcanzados con
+                # un consto de camino menor
+                if nuevo_estado not in alcanzados or nuevo_costo < alcanzados[nodo.state]:
+                    nuevo_nodo = Node('', nuevo_estado, nuevo_costo, nodo, accion)
+                    alcanzados[nuevo_estado] = nuevo_costo
+                    frontera.add(nuevo_nodo, nodo.cost + heuristica(nuevo_nodo)) 
         
-        return NoSolution(explored)
+        # Devuelve 'NoSolution' porque frontera está vacío
+        return NoSolution(alcanzados)
